df1a78b0e9cb0b53eb3961b191bdffb3bf8acd4b
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2026-04-01 07:34:11 +0700

    initial saldo to 0

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -1,5 +1,5 @@
 // Saldo Awal Kas (Tunai + Bank)
-export const INITIAL_BANK =  24370386;
-export const INITIAL_TUNAI = 364000;
+export const INITIAL_BANK =  0;
+export const INITIAL_TUNAI = 0;
 
 export const TOTAL_INITIAL_BALANCE = INITIAL_BANK + INITIAL_TUNAI;
