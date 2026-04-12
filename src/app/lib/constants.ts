547841424fe284f2219b525553fc22700b88ad33
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2026-04-12 19:47:29 +0700

    update balance

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -1,5 +1,5 @@
 // Saldo Awal Kas (Tunai + Bank)
-export const INITIAL_BANK =  30186408;
+export const INITIAL_BANK =  24576055;
 export const INITIAL_TUNAI = 364000;
 
 export const TOTAL_INITIAL_BALANCE = INITIAL_BANK + INITIAL_TUNAI;
